8ab2fc53fe55fe7dd506dca878aaf0d3633f1422
Nick Mitchell <576254+mightyhorst@users.noreply.github.com>
2025-04-23 15:58:08 +1000

    📚 docs: github action to publish to Github Pages

---
+++ docs/App.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+/**
+ * @styles
+ */
+import './App.css';
+export default function App(): React.JSX.Element;
+//# sourceMappingURL=App.d.ts.map
---
+++ docs/components/HelloWorld.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+export declare function HelloWorld(): React.JSX.Element;
+export default HelloWorld;
+//# sourceMappingURL=HelloWorld.d.ts.map
